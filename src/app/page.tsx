d3970a7d0d16ff8e57fcb285def12f37b1b888c1
zen-hideyuki <h.kitakata@zenplace.co.jp>
2025-02-21 17:35:11 +0900

    test: Add title for vercel deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className={`mb-3 text-2xl font-semibold`}>
+          Vercel Deploy Test{' '}
+        </h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
